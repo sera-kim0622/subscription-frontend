f7b48a31ad8c68709e69e42d9d4ac73d7ca586ee
sera-kim <sera.kim0622@gmail.com>
2026-01-06 22:48:10 +0900

    fix(): 회원가입 상태 컴포넌트 적용

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,3 +1,4 @@
+import AuthInput from "../../components/auth/AuthInput/AuthInput";
 import AuthLayout from "../../components/auth/AuthLayout/AuthLayout";
 import styles from "./Signup.module.css";
 import { useState } from "react";
@@ -18,34 +19,20 @@ function Signup() {
             이미 계정이 있으신가요? <a href="/login">로그인</a>
           </>
         }>
-        <div className={styles.field}>
-          <input
-            value={email}
-            type="text"
-            placeholder="이메일"
-            onChange={e => setEmail(e.target.value)}
-          />
-        </div>
+        <AuthInput value={email} placeHolder="이메일" onChange={e => setEmail(e.target.value)} />
 
-        <div className={styles.field}>
-          <input
-            value={password}
-            type="password"
-            placeholder="비밀번호"
-            onChange={e => setPassword(e.target.value)}
-          />
-        </div>
+        <AuthInput
+          value={password}
+          type="password"
+          placeHolder="비밀번호"
+          onChange={e => setPassword(e.target.value)}
+        />
 
-        <div className={styles.field}>
-          <input
-            value={nickname}
-            type="text"
-            placeholder="닉네임"
-            onChange={e => setNickname(e.target.value)}
-          />
-        </div>
-
-        <div className={styles.field}>{/* 에러 메시지 자리 */}</div>
+        <AuthInput
+          value={nickname}
+          placeHolder="닉네임"
+          onChange={e => setNickname(e.target.value)}
+        />
 
         <button className={styles.button}>회원가입</button>
       </AuthLayout>
