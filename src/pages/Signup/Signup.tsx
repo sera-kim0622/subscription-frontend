aa69a23c14ca0007237a39fdaa168bf7c2ab08fa
sera-kim <sera.kim0622@gmail.com>
2026-01-06 17:54:08 +0900

    feat(): 초기 회원가입 페이지 작성

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
 function Signup() {
-  return <></>;
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  const [nickname, setNickname] = useState("");
+
+  return (
+    <>
+      <div style={{ maxWidth: 400, margin: "0 auto" }}>
+        <h2>회원가입</h2>
+        <input
+          value={email}
+          type="text"
+          placeholder="이메일"
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <input
+          value={password}
+          type="password"
+          placeholder="비밀번호"
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <input
+          value={nickname}
+          type="text"
+          placeholder="닉네임"
+          onChange={e => setNickname(e.target.value)}
+        />
+      </div>
+    </>
+  );
 }
 
 export default Signup;
