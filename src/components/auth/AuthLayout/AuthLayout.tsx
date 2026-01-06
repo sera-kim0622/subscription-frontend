c8949a31cffbd519cfd1c79d95a9d97c6aa026b0
sera-kim <sera.kim0622@gmail.com>
2026-01-06 18:31:42 +0900

    feat(): 회원가입 & 로그인 공통 컴포넌트 작성

---
+++ src/components/auth/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,23 @@
+import styles from "./AuthLayout.module.css";
+
+interface AuthLayoutProps {
+  title: string;
+  children: React.ReactNode;
+  footer: React.ReactNode;
+}
+
+function AuthLayout({ title, children, footer }: AuthLayoutProps) {
+  return (
+    <>
+      <div className={styles.page}>
+        <div className={styles.card}>
+          <h1 className={styles.title}>{title}</h1>
+          <div className={styles.card}>{children}</div>
+          <div className={styles.footer}>{footer}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AuthLayout;
