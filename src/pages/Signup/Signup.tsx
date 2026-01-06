59e616aba7309946b471de0bf385dfd94fbac7df
sera-kim <sera.kim0622@gmail.com>
2026-01-06 18:13:10 +0900

    fix(): 회원가입 - tsx 스타일 적용

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,3 +1,4 @@
+import styles from "./Signup.module.css";
 import { useState } from "react";
 
 function Signup() {
@@ -9,28 +10,47 @@ function Signup() {
 
   return (
     <>
-      <div style={{ maxWidth: 400, margin: "0 auto" }}>
-        <h2>회원가입</h2>
-        <input
-          value={email}
-          type="text"
-          placeholder="이메일"
-          onChange={e => setEmail(e.target.value)}
-        />
+      <div className={styles.page}>
+        <div className={styles.card}>
+          <h1 className={styles.title}>회원가입</h1>
+          <div className={styles.form}>
+            <div className={styles.field}>
+              <input
+                value={email}
+                type="text"
+                placeholder="이메일"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
 
-        <input
-          value={password}
-          type="password"
-          placeholder="비밀번호"
-          onChange={e => setPassword(e.target.value)}
-        />
+            <div className={styles.field}>
+              <input
+                value={password}
+                type="password"
+                placeholder="비밀번호"
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
 
-        <input
-          value={nickname}
-          type="text"
-          placeholder="닉네임"
-          onChange={e => setNickname(e.target.value)}
-        />
+            <div className={styles.field}>
+              <input
+                value={nickname}
+                type="text"
+                placeholder="닉네임"
+                onChange={e => setNickname(e.target.value)}
+              />
+            </div>
+
+            <div className={styles.field}>{/* 에러 메시지 자리 */}</div>
+
+            <button className={styles.button}>회원가입</button>
+
+            <div className={styles.footer}>
+              이미 회원가입을 하셨나요?
+              <a href="/signin">로그인</a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
