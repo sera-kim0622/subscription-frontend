f5a803b574afd918d6056c63de1096d3f77a3cf4
sera-kim <sera.kim0622@gmail.com>
2026-01-06 22:46:47 +0900

    fix(): 로그인 상태 컴포넌트 적용

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,4 @@
+import AuthInput from "../../components/auth/AuthInput/AuthInput";
 import AuthLayout from "../../components/auth/AuthLayout/AuthLayout";
 import styles from "./Login.module.css";
 import { useState } from "react";
@@ -10,31 +11,20 @@ function Login() {
   return (
     <>
       <AuthLayout
-        title={"회원가입"}
+        title={"로그인"}
         footer={
           <>
             아직 계정이 없으신가요? <a href="/signup">회원가입</a>
           </>
         }>
-        <div className={styles.field}>
-          <input
-            value={email}
-            type="text"
-            placeholder="이메일"
-            onChange={e => setEmail(e.target.value)}
-          />
-        </div>
+        <AuthInput value={email} placeHolder="이메일" onChange={e => setEmail(e.target.value)} />
 
-        <div className={styles.field}>
-          <input
-            value={password}
-            type="password"
-            placeholder="비밀번호"
-            onChange={e => setPassword(e.target.value)}
-          />
-        </div>
-
-        <div className={styles.field}>{/* 에러 메시지 자리 */}</div>
+        <AuthInput
+          value={password}
+          type="password"
+          placeHolder="비밀번호"
+          onChange={e => setPassword(e.target.value)}
+        />
 
         <button className={styles.button}>로그인</button>
       </AuthLayout>
