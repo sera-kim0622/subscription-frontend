d56c7c3636f6fa02073dae8d5d206ad596c948ac
sera-kim <sera.kim0622@gmail.com>
2026-01-06 18:20:29 +0900

    feat(): login페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<Navigate to="/signup" />} />
     </Routes>
   );
