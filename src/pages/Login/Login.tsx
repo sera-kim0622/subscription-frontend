579c68cacf147876bdd398fff8cedf29e942780c
sera-kim <sera.kim0622@gmail.com>
2026-01-06 18:18:54 +0900

    feat(): 로그인 초기 설정

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,48 @@
+import styles from "./Signup.module.css";
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <div className={styles.page}>
+        <div className={styles.card}>
+          <h1 className={styles.title}>회원가입</h1>
+          <div className={styles.form}>
+            <div className={styles.field}>
+              <input
+                value={email}
+                type="text"
+                placeholder="이메일"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className={styles.field}>
+              <input
+                value={password}
+                type="password"
+                placeholder="비밀번호"
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className={styles.field}>{/* 에러 메시지 자리 */}</div>
+
+            <button className={styles.button}>로그인</button>
+
+            <div className={styles.footer}>
+              이미 회원가입을 하셨나요?
+              <a href="/signup">회원가입</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
