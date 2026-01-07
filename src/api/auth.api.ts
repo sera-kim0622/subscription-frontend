1be435d22be0c7e0a22f3ce3a96904d28600a413
sera-kim <sera.kim0622@gmail.com>
2026-01-07 15:22:53 +0900

    feat(): 회원가입, 로그인 백엔드 호출 함수 작성

---
+++ src/api/auth.api.ts
@@ -0,0 +1,25 @@
+import { api } from "./axios";
+
+export enum UserRole {
+  ADMIN = "ADMIN",
+  USER = "USER",
+}
+
+export interface SignupRequest {
+  email: string;
+  password: string;
+  role: UserRole;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export const signup = (data: SignupRequest) => {
+  return api.post("/users", data);
+};
+
+export const login = (data: LoginRequest) => {
+  return api.post("/users/login", data);
+};
