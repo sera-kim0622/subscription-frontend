1d1bfa67cd80a6dcfe41e8f71dfb511062a8eeee
sera-kim <sera.kim0622@gmail.com>
2026-01-07 23:05:46 +0900

    feat(): 요금제 페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
+import Products from "./pages/Products/Products";
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/products" element={<Products />} />
       <Route path="*" element={<Navigate to="/signup" />} />
     </Routes>
   );
