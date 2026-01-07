64fa9ff11b6c669633980c443cd8fc142942ce10
sera-kim <sera.kim0622@gmail.com>
2026-01-07 15:35:46 +0900

    fix(): 환경변수명 변경 - vite는 VITE_접두사로 된 변수만 설정가능

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
