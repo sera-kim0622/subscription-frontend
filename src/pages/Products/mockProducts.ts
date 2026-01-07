8cfaeaa8780e60f64eb539c5758fa60c4252c52e
sera-kim <sera.kim0622@gmail.com>
2026-01-07 23:30:07 +0900

    feat(): 상품 설정을 위한 mock파일

---
+++ src/pages/Products/mockProducts.ts
@@ -0,0 +1,29 @@
+export type ProductType = "MONTHLY" | "YEARLY";
+
+export interface Product {
+  id: number;
+  name: string;
+  type: ProductType;
+  price: number;
+}
+
+export const mockProducts: Product[] = [
+  {
+    id: 1,
+    name: "Basic",
+    type: "MONTHLY",
+    price: 10000,
+  },
+  {
+    id: 2,
+    name: "Pro",
+    type: "MONTHLY",
+    price: 30000,
+  },
+  {
+    id: 3,
+    name: "Enterprise",
+    type: "YEARLY",
+    price: 0,
+  },
+];
