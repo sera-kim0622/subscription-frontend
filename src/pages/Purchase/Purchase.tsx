a800b2ea5f9895c56be193ce3365fe71117336fa
sera-kim <sera.kim0622@gmail.com>
2026-01-09 15:29:07 +0900

    feat(): 구매페이지 작성

---
+++ src/pages/Purchase/Purchase.tsx
@@ -0,0 +1,61 @@
+import styles from "./Purchase.module.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import { purchase } from "../../api/purchase.api";
+import { Product } from "../../api/product.api";
+
+const Purchase = () => {
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const product: Product = state?.product;
+
+  if (!product) {
+    navigate("/products");
+    return null;
+  }
+
+  const handlePurchase = async () => {
+    try {
+      await purchase({ productId: product.id, simulate: "success" });
+
+      alert("구매가 완료되었습니다.");
+      navigate("./products");
+    } catch (err) {
+      console.log(err);
+      alert("구매에 실패하였습니다.");
+    }
+  };
+
+  return (
+    <div className={styles["page"]}>
+      <h1 className={styles["title"]}>결제하기</h1>
+
+      <div className={styles["product-box"]}>
+        <div className={styles["product-name"]}>{product.name}</div>
+        <div className={styles["product-price"]}>₩{product.price}</div>
+      </div>
+
+      <div className={styles["payment-box"]}>
+        <h2 className={styles["section-title"]}>결제 수단</h2>
+
+        <label className={styles["radio"]}>
+          <input type="radio" checked readOnly />
+          Credit Card
+        </label>
+
+        <div className={styles["card-form"]}>
+          <input placeholder="카드번호" disabled />
+          <div className={styles["row"]}>
+            <input placeholder="MM/YY" disabled />
+            <input placeholder="CVC" disabled />
+          </div>
+        </div>
+
+        <button className={styles["purchase-button"]} onClick={handlePurchase}>
+          결제하기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Purchase;
